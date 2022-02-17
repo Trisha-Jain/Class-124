0fd027d4bd5fc7ef133fbad3f909469b3a3d664e
Trisha <83487927+Trisha-Jain@users.noreply.github.com>
2022-02-17 20:27:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 550);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+}
+
+function draw() {
+    background('#969697');
+
+    document.getElementById("square_side").innerHTML = "Weight and Height of the square will be = " +difference + "px";
+    fill('#F90093');
+    stroke('#F90093 ');
+    square(noseX, noseY, difference);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose X = " + noseX + "nose Y = " + noseY);
+
+        leftWristX = results[0].pose.leftWristX;
+        rightWristX = results[0].pose.rightWristX;
+        difference = floor(leftWristX - rightWristX);
+        console.log("left wrist = " + leftWristX + " right wrist = " + rightWristX + "difference = " + difference);
+
+    }
+}
